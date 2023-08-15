c8ef0f65c1536e1b8dfaa08385a0e24bb29caed7
Mohamed AL-Shamali <M.AL-Shamali@sedco-online.com>
2023-08-15 10:13:09 +0300

    I think it works

--- WebFileworxClient/Web Fileworx Client/EditBeforeRun.cs
+++ WebFileworxClient/Web Fileworx Client/EditBeforeRun.cs
@@ -3,10 +3,10 @@
     public class EditBeforeRun
     {
         //Directorys of the Program Saved files
-        public const string savedusersFiles = @"C:\Users\M.AL-Shamali\Desktop\Web Fileworx Client\Users";
-        public const string newsFiles = @"C:\Users\M.AL-Shamali\Desktop\Web Fileworx Client\News";
-        public const string ImagesFiles = @"C:\Users\M.AL-Shamali\Desktop\Web Fileworx Client\WebFileworxClient\Web Fileworx Client\wwwroot\Images\News Images";
-        public const string ShowedImage = @"C:\Users\M.AL-Shamali\Desktop\Web Fileworx Client\WebFileworxClient\Web Fileworx Client\wwwroot\Images\ShowedImage\AddImage";
+        public const string savedusersFiles = @"C:\Users\M.AL-Shamali\Desktop\Demo Projects\Web Fileworx Client\Users";
+        public const string newsFiles = @"C:\Users\M.AL-Shamali\Desktop\Demo Projects\Web Fileworx Client\News";
+        public const string ImagesFiles = @"C:\Users\M.AL-Shamali\Desktop\Demo Projects\Web Fileworx Client\WebFileworxClient\Web Fileworx Client\wwwroot\Images\News Images";
+        public const string ShowedImage = @"C:\Users\M.AL-Shamali\Desktop\Demo Projects\Web Fileworx Client\WebFileworxClient\Web Fileworx Client\wwwroot\Images\ShowedImage\AddImage";
 
         //Complex Seperator
         public static string[] Seperator = { "%%$$##" };
