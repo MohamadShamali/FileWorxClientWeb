1b5b78f475e5efb9d5b5717e9960e33b24de34aa
Mohamed AL-Shamali <M.AL-Shamali@sedco-online.com>
2023-08-16 14:53:44 +0300

    all works

--- WebFileworxClient/Web Fileworx Client/EditBeforeRun.cs
+++ WebFileworxClient/Web Fileworx Client/EditBeforeRun.cs
@@ -3,12 +3,6 @@
     public class EditBeforeRun
     {
         //Directorys of the Program Saved files
-        public const string savedusersFiles = @"C:\Users\M.AL-Shamali\Desktop\Demo Projects\Web Fileworx Client\Users";
-        public const string newsFiles = @"C:\Users\M.AL-Shamali\Desktop\Demo Projects\Web Fileworx Client\News";
         public const string ImagesFiles = @"C:\Users\M.AL-Shamali\Desktop\Demo Projects\Web Fileworx Client\WebFileworxClient\Web Fileworx Client\wwwroot\Images\StoredImages";
-        public const string ShowedImage = @"C:\Users\M.AL-Shamali\Desktop\Demo Projects\Web Fileworx Client\WebFileworxClient\Web Fileworx Client\wwwroot\Images\ShowedImage\StoredImages";
-
-        //Complex Seperator
-        public static string[] Seperator = { "%%$$##" };
     }
 }
